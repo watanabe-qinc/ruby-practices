05a228bfe3a7c37e1edc74c906de25c276bdf337
watanabe-qinc <konami.watanabe@qinc.co.jp>
2024-09-25 18:09:23 +0900

    変数名・メソッド名の変更と処理の簡略化

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,36 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def make_ls_arrays(dir)
-  directory_contents = dir
-
+def devide_directory_contents(directory_contents)
   column = 3
-  if (directory_contents.length % column).zero?
-    directory_contents.each_slice(directory_contents.length / column).to_a
-  else
-    directory_contents.each_slice(directory_contents.length / column + 1).to_a
+
+  devided_directory_contents =
+    if (directory_contents.length % column).zero?
+      directory_contents.each_slice(directory_contents.length / column).to_a
+    else
+      directory_contents.each_slice(directory_contents.length / column + 1).to_a
+    end
+
+  devided_directory_contents.last << '' while devided_directory_contents.last.length < devided_directory_contents.first.length
+  devided_directory_contents
+end
+
+def sort_and_puts_directory_contents(devided_directory_contents, directory_contents)
+  max_length = directory_contents.map(&:length).max
+
+  devided_directory_contents.transpose.each do |transposed_directory_contents|
+    puts transposed_directory_contents.map { |transposed_directory_content| transposed_directory_content.ljust(max_length, ' ') }.join(' ' * 5)
   end
 end
 
-no_option_ls_arrays = make_ls_arrays(Dir.glob('*'))
-
-def sort_ls_arrays(arrays)
-  arrays.last << '' while arrays.last.length < arrays.first.length
-
-  max_length = 0
-  arrays.flatten.map do |character|
-    character_length = character.length
-
-    max_length = character_length if max_length < character_length
-  end
-
-  arrays.transpose.each do |array|
-    list =
-      array.map do |name|
-        name.ljust(max_length, ' ')
-      end
-    puts list.join(' ' * 5)
-  end
-end
-
-sort_ls_arrays(no_option_ls_arrays)
+sort_and_puts_directory_contents(devide_directory_contents(Dir.glob('*')), Dir.glob('*'))
